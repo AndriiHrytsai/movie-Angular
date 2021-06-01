c537c474b5d1e921436267f06b325c03e569c3d0
AndriiHrytsai <andriyhrytsay@gmail.com>
2021-06-02 02:11:42 +0300

    done movieInfo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,20 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {MoviesComponent} from './components/movies/movies.component';
-import {MovieComponent} from './components/movie/movie.component';
+import {MovieComponent} from './components/movies/movie/movie.component';
 import {HttpClientModule} from "@angular/common/http";
-import { HeaderComponent } from './components/header/header.component';
+import {HeaderComponent} from './components/header/header.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from './components/home/home.component';
+import {LikeComponent} from './components/like/like.component';
+import {MovieDetailsComponent} from './components/movies/movie-details/movie-details.component';
+
+let routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'movies', component: MoviesComponent},
+  {path: 'movies/:id', component: MovieDetailsComponent},
+  {path: 'like', component: LikeComponent}
+]
 
 @NgModule({
   declarations: [
@@ -13,10 +24,14 @@ import { HeaderComponent } from './components/header/header.component';
     MoviesComponent,
     MovieComponent,
     HeaderComponent,
+    HomeComponent,
+    LikeComponent,
+    MovieDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
